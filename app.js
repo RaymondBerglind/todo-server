70d3b3e6ca897dbec5d3d3c4220e2f5607c4eb6f
Raymond Berglind <raymond.berglind@evry.com>
2020-04-19 09:31:18 +0200

    Added completely open CORS policy.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 9000;
 const mainController = require('./src/controllers/main');
 
+app.use(cors());
 app.use(mainController);
 
 app.listen(port, () => console.log('Todo server running at http://localhost:', port));
