dc8e913c7b9bd1a7c22164b6254084e944bcd2c0
Raymond Berglind <raymond.berglind@evry.com>
2020-06-08 19:52:07 +0200

    Added Mocha, along with a passing dummy test.

---
+++ src/test/dummySpec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('REMOVEME: Dummy test suite', function () {
+    describe('REMOVEME: Dummy test', function () {
+        it('Should be removed, but should also succeed.', function () {
+            assert.equal(true, true);
+        });
+    });
+});
